0ea64cadf55a885e19fb1f7e64327c32d1790466
s4magent <s4m.agency@gmail.com>
2025-01-14 17:44:37 +0200

    Add files via upload

---
+++ TelegramBot/bot.py
@@ -0,0 +1,23 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+# Ваш токен
+telegram_token = "7378387007:AAEz8lMqQaNhnJk90U6HkxjcJXvcX-5bxO4
+"
+
+# Функция, которая будет отвечать на команду /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply("Привет! Я твой бот.")
+
+# Основная функция
+def main() -> None:
+    application = Application.builder().token(telegram_token).build()
+
+    # Регистрируем обработчик команды /start
+    application.add_handler(CommandHandler("start", start))
+
+    # Запускаем бота
+    application.run_polling()
+
+if __name__ == "__main__":
+    main()
