a467969c6ff1876e5207b82cde76c626631a03d4
s4magent <s4m.agency@gmail.com>
2025-01-14 17:54:05 +0200

    Update bot.py

--- TelegramBot/bot.py
+++ TelegramBot/bot.py
@@ -4,10 +4,12 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 # Ваш токен
 telegram_token = "7378387007:AAEz8lMqQaNhnJk90U6HkxjcJXvcX-5bxO4"
 
-
 # Функция, которая будет отвечать на команду /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply("Привет! Я твой бот.")
+    if update.message:
+        await update.message.reply("Привет! Я твой бот.")
+    else:
+        print("Нет сообщения!")
 
 # Основная функция
 def main() -> None:
